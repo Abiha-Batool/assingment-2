608d802542dcee77a1cef5c8632a8a6d570584ac
Abiha-Batool <abiha.nedacad@gmail.com>
2024-12-18 14:54:22 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,82 @@
+// // create variable name assign your name
+// let name="abiha"
+// console.log("Hello, my name is",name);
+
+// // declare two numbers
+// let a=5;
+// let b=3;
+// let sum=a+b
+// console.log("the sum of a and b is ",sum);
+// let difference=(a-b);
+// console.log("the difference of a and b",difference);
+// let multiply=(a*b);
+// console.log("the multiply of a and b",multiply);
+
+// // odd
+// let d=9;
+// let number=d%2==0?
+// "even number":"odd number"
+// console.log(number);
+
+// // even
+// let f=8;
+// let abiha=f%2==0?
+// "even number":"odd number"
+// console.log(abiha);
+
+// // number is greater than 10
+// let num=10
+// let c= num>10?"she in under weight":"she is in weight";
+// console.log(c);
+
+// // vote
+// let k=17
+// let p= num>17?"can vote":"cannot vote"
+// console.log(p);
+
+
+//  18-dec-2024
+// person is eligible
+
+//  let per=15;
+//  let abc = per>18 ?
+//  "eligible":"not"
+//  console.log(abc);
+
+// //  positive or negative
+// let d=-9;
+// let number = d>0 ?
+// "positive":"negative"
+// console.log(number);
+
+// // variables differ types
+// let accountId="abiha";
+// console.log("type of accountId is",typeof(accountId))
+// let accountPassword="1234";
+// console.log("type of accountPassword is ",typeof(accountPassword))
+// let phonenumber=231568
+// console.log("type of phonenumber is",typeof(phonenumber))
+// console.table({accountId,accountPassword,phonenumber});
+
+// // total cost
+// let cost1=67
+// let cost2=647
+// let cost3=228
+// let cost4=830
+// let sum=cost1+cost2+cost3+cost4
+// console.log( "cost1,cost2,cost3,cost4",sum)
+
+// // perimeter of rectangle
+// let l=5
+// let b=4
+// let abc= 2*l+2*b
+// console.log("the perimeter of rectangle",abc)
+
+// // position of space
+// let spicy="this is a string _ is"
+// console.log("the position of _ is",spicy.indexOf(" "))
+
+//  sub String
+
+
+
