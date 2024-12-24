54d99145b816712da577feed4d5bcf6b0f5e536f
Abiha-Batool <abiha.nedacad@gmail.com>
2024-12-24 14:51:37 +0500

    Add files via upload

---
+++ function.js
@@ -0,0 +1,109 @@
+//Q. to add three numbers
+// function addThreeNumber(num1,num2,num3){
+//    return (num1+num2+num3)
+// }
+// addThreeNumber(3,5,2)
+// console.log((addThreeNumber(3,5,2)))
+
+// Q. take 3 arguments first 2 params multiplied by each other and last param is divided by the result
+
+// function arguments(argu1,argu2,argu3){
+//     product = argu1*argu2
+//     result = product/argu3
+//     return result
+
+// }
+// console.log((arguments(4,2,2)))
+
+// Q.greet
+// function greeting(name){
+// console.log (`hello this is a good class ${name}`)
+// }
+// greeting("Abiha")
+
+//Q.square of a number
+// function square(number){
+//     squareNum=number**2 
+//     return squareNum
+// }
+// square(5)
+// console.log(squareNum)
+
+//Q area of rectangle
+//  function rectangle(l,b){
+//     area=l*b
+//     return area
+//  }
+//  rectangle(8,2)
+//  console.log (`area of rectangle ${rectangle(8,2)}`)
+
+// Q check num positive negative or zero
+
+// 24-dec-2024
+// Q1..write a program that ask the user to input two numbers and calculates thier sum,difference,product and division.
+// function addNum(n1, n2){
+//     return n1+n2
+// }
+// function subNum(n1, n2){
+//     return n1-n2
+// }
+// function mulNum(n1, n2){
+//     return n1*n2
+// }
+// function divNum(n1, n2){
+//     return n1/n2
+// }
+
+
+// operation= prompt(`choose any one of them:add,sub,mul,div`)
+//  a=parseFloat(prompt(`enter 1st number`))
+//  b=parseFloat(prompt(`enter 2nd number`)) 
+ 
+//  if (operation==`add`){
+//     console.log(addNum(a,b))
+   
+//  }
+
+//  else if (operation==`sub`){
+//     console.log(subNum(a,b))
+//  }
+ 
+//  else if (operation==`mul`){
+//     console.log(mulNum(a,b))
+//  }
+ 
+//  else {divNum(a,b)}
+
+ 
+
+// Q2...write a program that ask the user for a temperature in celcius and converts it into fahrenheit.use the formula..(fahrenheit=celcius*(9/5)+32)
+
+// let celcius= prompt(`enter any temperature in celsius]`)
+// let fahrenhiet=celcius*(9/5)+32
+// alert(`the converted temperature in fahrenhietis ${fahrenhiet}`)
+
+// Q3...write a program that trakes age from the user 
+
+// function check_age(age1){
+//     if (age1<18){
+//         return "minor"
+//     }
+//     else if (age1>=18 && age1<60){
+//         return "adult"
+//     }
+//     else {
+//         return "senior citizen"
+//     }
+// }
+// age =prompt(`enter your age to judge`)
+// alert (check_age(age))
+
+
+
+
+
+
+
+
+
+
