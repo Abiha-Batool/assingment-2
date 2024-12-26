f9339d40b766b34fda2b4a26986c8e7f04852313
Abiha-Batool <abiha.nedacad@gmail.com>
2024-12-26 14:31:21 +0500

    Add files via upload

---
+++ object.js
@@ -0,0 +1,36 @@
+const person={
+    name:"abiha",
+    age:"17",
+    isStudent:"true",
+}
+person.age="20",
+person.property="hobby"
+person.name="Abiha batool"
+console.log(person)
+console.log(person.name)
+
+const carwithbrand={
+    model:"civic",
+    year:"2012",
+    owner:{
+        name:"Abiha Batool",
+        age:"20",
+        address:{
+            city:"Karchi",
+            zip:"15684",
+        }
+    },}
+carwithbrand.owner.address.city="Lahore"
+carwithbrand.age="26"
+carwithbrand.phone="2036684548"
+ console.log(carwithbrand.model)
+ console.log(carwithbrand.owner.address.city)
+
+
+
+
+
+   
+
+
+
